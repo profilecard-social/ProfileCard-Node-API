60af2948da7342d1eb096a5046c550c8f27d0680
Timeox2k <schockilp@gmail.com>
2023-09-17 00:32:45 +0200

    Fixed a crash

--- src/channel/userData.js
+++ src/channel/userData.js
@@ -13,42 +13,48 @@ const accessPath = config.access_path;
 
 export default (socket, body, callback) => {
 
-    authedChannel(socket, body, callback, async (user) => {
-        const nameHash = md5(user.name.toLowerCase());
-        const profilePicturePath = `${uploadDirectory}/${nameHash}.png`;
-        const backgroundImagePath = `${uploadDirectory}/bg_${nameHash}.png`;
-
-        let profilePicture;
-        let backgroundImage;
-
-        if (fs.existsSync(profilePicturePath)) {
-            profilePicture = `${accessPath}/${nameHash}.png`;
-        } else if (fs.existsSync(`${uploadDirectory}/${user.id}.png`)) {
-            profilePicture = `${accessPath}/${user.id}.png`;
-        } else {
-            profilePicture = `${accessPath}/default.png`;
-        }
-
-        if (fs.existsSync(backgroundImagePath)) {
-            backgroundImage = `${accessPath}/bg_${nameHash}.png`;
-        }  else {
-            backgroundImage = `${accessPath}/bg_default.png`;
-        }
-
-        const data = {
-            "icon": `${profilePicture}`,
-            "bgImage": `${backgroundImage}`,
-        };
-
-        const dataToTransfer = [ "name", "email", "status", "buttoncolor", "textcolor", "bgcolor", "theme", "lang", "last_seen_at", "icon", "bgImage" ];
-        for (let [key, value] of Object.entries(user)) {
-            if (dataToTransfer.includes(key)) {
-                data[key] = value;
+    try {
+        authedChannel(socket, body, callback, async (user) => {
+            const nameHash = md5(user.name.toLowerCase());
+            const profilePicturePath = `${uploadDirectory}/${nameHash}.png`;
+            const backgroundImagePath = `${uploadDirectory}/bg_${nameHash}.png`;
+    
+            let profilePicture;
+            let backgroundImage;
+    
+            if (fs.existsSync(profilePicturePath)) {
+                profilePicture = `${accessPath}/${nameHash}.png`;
+            } else if (fs.existsSync(`${uploadDirectory}/${user.id}.png`)) {
+                profilePicture = `${accessPath}/${user.id}.png`;
+            } else {
+                profilePicture = `${accessPath}/default.png`;
             }
-        }
+    
+            if (fs.existsSync(backgroundImagePath)) {
+                backgroundImage = `${accessPath}/bg_${nameHash}.png`;
+            }  else {
+                backgroundImage = `${accessPath}/bg_default.png`;
+            }
+    
+            const data = {
+                "icon": `${profilePicture}`,
+                "bgImage": `${backgroundImage}`,
+            };
+    
+            const dataToTransfer = [ "name", "email", "status", "buttoncolor", "textcolor", "bgcolor", "theme", "lang", "last_seen_at", "icon", "bgImage" ];
+            for (let [key, value] of Object.entries(user)) {
+                if (dataToTransfer.includes(key)) {
+                    data[key] = value;
+                }
+            }
+    
+            success(callback, { body: data });
+        });
+    } catch(error) {
+        console.log(error);
+    }
 
-        success(callback, { body: data });
-    });
+
 
 }
 
--- src/response/Response.js
+++ src/response/Response.js
@@ -1,7 +1,15 @@
 export function success(callback, { code = 200, success = true, message = "Success", body = {} }) {
-    callback( { code: code, success: success, message: message, body: body } )
+    try {
+        callback( { code: code, success: success, message: message, body: body } )
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export function fail(callback, { code, success = false, message = "", body = {} }) {
-    callback( { code: code, success: success, message: message, body: body } )
+    try {
+        callback( { code: code, success: success, message: message, body: body } )
+    } catch (error) {
+        console.log(error);
+    }
 }
