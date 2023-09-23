6b15f5f94f2226c26d87046390c8d7b2fe872129
Timeox2k <schockilp@gmail.com>
2023-09-23 06:32:56 +0200

    Update updateValue.js

--- src/channel/updateValue.js
+++ src/channel/updateValue.js
@@ -6,26 +6,30 @@ import authedChannel from "./generic/authedChannel.js";
 
 export default async (socket, body, callback) => {
 
-    authedChannel(socket, body, callback, async (user) => {
+    try {
+        authedChannel(socket, body, callback, async (user) => {
 
-        const key = body.key;
-        const value = body.value;
-
-        if (!key || !value) {
-            fail(callback, Codes.WrongArguments);
-            return;
-        }
-
-        const allowedKeys = [ "status", "buttoncolor", "textcolor", "bgcolor", "theme", "lang" ];
-
-        if (!allowedKeys.includes(key)) {
-            fail(callback, Codes.Forbidden);
-            return;
-        }
-
-        await updateUserParam("token", body.token, key, value);
-        success(callback, Codes.Success);
-
-    });
+            const key = body.key;
+            const value = body.value;
+    
+            if (!key || !value) {
+                fail(callback, Codes.WrongArguments);
+                return;
+            }
+    
+            const allowedKeys = [ "status", "buttoncolor", "textcolor", "bgcolor", "theme", "lang" ];
+    
+            if (!allowedKeys.includes(key)) {
+                fail(callback, Codes.Forbidden);
+                return;
+            }
+    
+            await updateUserParam("token", body.token, key, value);
+            success(callback, Codes.Success);
+    
+        });
+    } catch (error) {
+        console.log(error);
+    }
 
 }
